832472ec9577b353c1bf904a33a5620d35bcdafa
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-03 09:38:01 +0100

    Set up CORS in backend and tested frontend connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+const options = { origin: "http://localhost:3000", useSuccessStatus: 200 };
 
+app.use(cors(options));
 app.get("/", (req, res) => {
   res.send("welcome from home");
 });
