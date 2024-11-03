7f88a3f4725a7d71ed670f866a18e74d904b158b
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-03 14:03:29 +0100

    fix database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
 // Connect to MongoDB
-exports.connectToDatabase = () => {
+exports.connectToDatabase = async () => {
   const uri = process.env.DATABASE_URL;
   const clientOptions = {
     serverApi: { version: "1", strict: true, deprecationErrors: true },
   };
-  const run = async () => {
     try {
       await mongoose.connect(uri, clientOptions);
       console.log("Connected to MongoDB Successfully");
-    } finally {
-      await mongoose.disconnect();
-    }
-  };
-  run().catch((err) => console.error("Failed to connect to MongoDB", err));
+    } catch(err) {
+        console.error("Error connecting to MongoDB:", err.message);
+  
+   }
+
 };
