320540924eeee1d36e79e577c3292d4e62c7a0df
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-03 11:07:14 +0100

    use Enivrements variable .env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const { readdirSync } = require("fs");
+
+dotenv.config();
 const app = express();
 //const options = { origin: "http://localhost:3000", useSuccessStatus: 200 };
 app.use(cors());
@@ -15,6 +18,5 @@ app.get("/", (req, res) => {
 app.get("/books", (req, res) => {
   res.send("hello");
 });
-app.listen(8000, () => {
-  console.log("server is listening...");
-});
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
