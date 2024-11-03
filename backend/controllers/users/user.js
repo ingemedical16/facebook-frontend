56a5a723ad7ff833c15e41829d5fb927fca4497c
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-03 14:50:54 +0100

    feat: add utility functions for user input validation

--- backend/controllers/users/user.js
+++ backend/controllers/users/user.js
@@ -1,3 +1,4 @@
+const { validateEmail, validateLength, validateUsername } = require("../../helpers/validate");
 const User = require("../../models/user/User");
 exports.register = async (req, res) => {
   const {
@@ -11,6 +12,25 @@ exports.register = async (req, res) => {
     birth_year_month,
     birth_year_day,
   } = req.body;
+  if(!validateEmail(email)){
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exists" });
+  }
+  if(!validateLength(first_name,2,30)){
+    return res.status(400).json({ message: "First name must be between 3 and 30 characters" });
+  }
+  if(!validateLength(last_name,2,30)){
+    return res.status(400).json({ message: "Last name must be between 3 and 30 characters" });
+  }
+  if(!validatePassword(password)){
+    return res.status(400).json({ message: "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character" });
+  }
+  if(!validateUsername(username)){
+    return res.status(400).json({ message: "Username must be between 3 and 30 characters and contain only alphanumeric characters and underscores" });
+  }
   try {
     const user = new User({
         first_name,
@@ -27,7 +47,7 @@ exports.register = async (req, res) => {
       res.status(200).json(user);
     
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
  
 };
