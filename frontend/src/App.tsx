ba396ed2d689957c2f3f9b156a736dfdecba13ab
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-11 21:45:13 +0100

    fix waring router error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Login from "./pages/login";
 import Home from "./pages/home";
 
 const App: React.FC = () => (
-  <Router>
+  <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true  }}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
