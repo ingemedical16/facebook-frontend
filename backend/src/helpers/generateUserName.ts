cbdb318e3b7a64c8f19e05889cfa9c25a72a68ba
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-19 22:01:26 +0100

    remove space from  autoGenerateUsername

--- backend/src/helpers/generateUserName.ts
+++ backend/src/helpers/generateUserName.ts
@@ -1,11 +1,15 @@
 import User from "../models/user/User";
 
 export const autoGenerateUsername = async (firstName: string, lastName: string): Promise<string> => {
-    const username = `${firstName.toLowerCase()}${lastName.toLowerCase()}${Date.now().toString().slice(-5)}`;
+    // Remove spaces from firstName and lastName
+    const cleanFirstName = firstName.replace(/\s/g, "");
+    const cleanLastName = lastName.replace(/\s/g, "");
+
+    const username = `${cleanFirstName.toLowerCase()}${cleanLastName.toLowerCase()}${Date.now().toString().slice(-5)}`;
 
     const existingUsername = await User.findOne({ username });
     if (existingUsername) {
-        return autoGenerateUsername(firstName, lastName);
+        return autoGenerateUsername(cleanFirstName, cleanLastName);
     }
     
     return username;
