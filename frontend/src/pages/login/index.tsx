ac23ae21ec1bea92edc4f67ddb2e72bd14069eb0
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-19 23:39:43 +0100

    update Login page

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,23 +1,15 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import Login from "../../components/Auth/login/Login";
 import Footer from "../../components/Auth/footer/Footer";
 import Register from "../../components/Auth/register/Register";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store";
-import { useNavigate } from "react-router-dom";
+
+
 
 const LoginPage: FC = () => {
-  const { user } = useSelector(
-    (state: RootState) => state.auth
-  );
-  const [visible, setVisible] = useState(false);
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (user) {
-      navigate("/");
-    }
-  }, [user, navigate]); 
+  const [visible, setVisible] = useState(false);
+  
+
 
   return (
     <div>
