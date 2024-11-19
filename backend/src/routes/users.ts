f4576425a0fccf8fd9615b386a2ce921a7d1a624
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-19 22:06:50 +0100

    Add isAuthenticated middlewares to this route "/verify-email"

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,11 +2,12 @@
 
 import express, { Router } from "express";
 import { register, verifyEmail,login } from "../controllers/user/user";
+import { isAuthenticated } from "../middlewares/authenticate";
 
 const router: Router = express.Router();
 
 router.post("/register", register);
-router.post("/verify-email", verifyEmail);
+router.post("/verify-email", isAuthenticated ,verifyEmail);
 router.post("/login", login);
 
 export default router;
