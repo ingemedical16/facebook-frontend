0082e5bdb0673f27abbc6f3bf2c4a2bcc611f904
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-19 22:23:10 +0100

    Update the login page

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,16 +1,31 @@
-import { FC } from "react";
-import Login from "../../components/Auth/login/Login"; // Correct import statement
+import { FC, useEffect, useState } from "react";
+import Login from "../../components/Auth/login/Login";
 import Footer from "../../components/Auth/footer/Footer";
 import Register from "../../components/Auth/register/Register";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage: FC = () => {
+  const { user } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const [visible, setVisible] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]); 
+
   return (
     <div>
-      <Login />
-      <Register/>
+      <Login setVisible={setVisible} />
+      {visible && <Register setVisible={setVisible} />}
       <Footer />
     </div>
-  )
+  );
 };
 
 export default LoginPage;
