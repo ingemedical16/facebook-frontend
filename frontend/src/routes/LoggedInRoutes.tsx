7573b25a4296e924f07634363b8470c1fdd7e131
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-20 21:23:59 +0100

    Add LoggedIn and NotLogIn routes

--- frontend/src/routes/LoggedInRoutes.tsx
+++ frontend/src/routes/LoggedInRoutes.tsx
@@ -1,13 +1,12 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
-import { Outlet,Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { FC } from "react";
-import LoginPage from "../pages/login";
 
 const LoggedInRoutes: FC = () => {
   const user = useSelector((state: RootState) => state.auth.user);
 
-  return user ? <Outlet /> : <LoginPage />;
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default LoggedInRoutes;
--- frontend/src/routes/NotLoggedInRoutes.tsx
+++ frontend/src/routes/NotLoggedInRoutes.tsx
@@ -1,13 +1,12 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
-import { Outlet,Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { FC } from "react";
 
-
-const LoggedInRoutes: FC = () => {
+const NotLoggedInRoutes: FC = () => {
   const user = useSelector((state: RootState) => state.auth.user);
 
   return user ? <Navigate to="/" /> : <Outlet />;
 };
 
-export default LoggedInRoutes;
+export default NotLoggedInRoutes;
