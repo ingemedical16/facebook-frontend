fbd9cfc663eb3b24a4fdbeda8486def5d77b5485
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-11-20 21:26:48 +0100

    Update Routes using LoggedInRoutes NotLoggedInRoutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,39 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Bounce, ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/login";
 import Home from "./pages/home";
 import VerifyEmail from "./components/Auth/verifyEmail/VerifyEmail";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 
 const App: React.FC = () => (
-
-  <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true  }}>
+  <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/verify-email/:verifyToken" element={<VerifyEmail />} />
+      <Route element={<LoggedInRoutes />}>
+        <Route path="/verify-email/:verifyToken" element={<VerifyEmail />} />
+        <Route path="/" element={<Home />} />
+      </Route>
+
+      <Route element={<NotLoggedInRoutes />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
     <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-        transition={Bounce}
-  />
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Bounce}
+    />
   </Router>
-
 );
 
 export default App;
