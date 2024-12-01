c99b539ff7e5cf1d1933c33268d97cd34a61363c
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-12-01 17:15:36 +0100

    Update post and user routes

---
+++ backend/src/routes/post.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import * as postController from '../controllers/post/post';
+import { isAuthenticated } from "../middlewares/authenticate";
+
+const router: Router = express.Router();
+
+router.post("/createPost", isAuthenticated, postController.createPost);
+router.get("/getAllPosts", isAuthenticated, postController.getAllPosts);
+router.put("/comment", isAuthenticated, postController.comment);
+router.put("/savePost/:id", isAuthenticated, postController.savePost);
+router.delete("/deletePost/:id", isAuthenticated, postController.deletePost);
+
+export default router;
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,57 +1,33 @@
 // src/routes/user.ts
 
 import express, { Router } from "express";
-import {
-  register,
-  verifyEmail,
-  login,
-  sendVerification,
-  searchUserByEmail,
-  sendResetPasswordCode,
-  validateResetCode,
-  changePassword,
-  search,
-  addToSearchHistory,
-  getSearchHistory,
-  removeFromSearchHistory,
-  getProfile,
-  updateProfilePicture,
-  updateCover,
-  updateDetails,
-  addFriend,
-  cancelRequest,
-  follow,
-  unfollow,
-  unfriend,
-  deleteRequest,
-  getFriendsPageInfos,
-} from "../controllers/user/user";
+import * as userController from "../controllers/user/user";
 import { isAuthenticated } from "../middlewares/authenticate";
 
 const router: Router = express.Router();
 
-router.post("/register", register);
-router.post("/verify-email", isAuthenticated, verifyEmail);
-router.post("/sendVerification", isAuthenticated, sendVerification);
-router.post("/search-user", searchUserByEmail);
-router.post("/login", login);
-router.post("/sendResetPasswordCode", sendResetPasswordCode);
-router.post("/validateResetCode", validateResetCode);
-router.post("/changePassword", changePassword);
-router.post("/search/:searchTerm",isAuthenticated, search);
-router.put("/addToSearchHistory", isAuthenticated, addToSearchHistory);
-router.get("/getSearchHistory", isAuthenticated, getSearchHistory);
-router.put("/removeFromSearchHistory", isAuthenticated, removeFromSearchHistory);
-router.get("/getProfile/:username", isAuthenticated, getProfile);
-router.put("/updateProfilePicture", isAuthenticated, updateProfilePicture);
-router.put("/updateCover", isAuthenticated, updateCover);
-router.put("/updateDetails", isAuthenticated, updateDetails);
-router.put("/addFriend/:id", isAuthenticated, addFriend);
-router.put("/cancelRequest/:id", isAuthenticated, cancelRequest);
-router.put("/follow/:id", isAuthenticated, follow);
-router.put("/unfollow/:id", isAuthenticated, unfollow);
-router.put("/unfriend/:id", isAuthenticated, unfriend);
-router.put("/deleteRequest/:id", isAuthenticated, deleteRequest);
-router.get("/getFriendsPageInfos", isAuthenticated, getFriendsPageInfos);
+router.post("/register", userController.register);
+router.post("/verify-email", isAuthenticated, userController.verifyEmail);
+router.post("/sendVerification", isAuthenticated, userController.sendVerification);
+router.post("/search-user", userController.searchUserByEmail);
+router.post("/login", userController.login);
+router.post("/sendResetPasswordCode", userController.sendResetPasswordCode);
+router.post("/validateResetCode", userController.validateResetCode);
+router.post("/changePassword", userController.changePassword);
+router.post("/search/:searchTerm",isAuthenticated, userController.search);
+router.put("/addToSearchHistory", isAuthenticated, userController.addToSearchHistory);
+router.get("/getSearchHistory", isAuthenticated, userController.getSearchHistory);
+router.put("/removeFromSearchHistory", isAuthenticated, userController.removeFromSearchHistory);
+router.get("/getProfile/:username", isAuthenticated, userController.getProfile);
+router.put("/updateProfilePicture", isAuthenticated, userController.updateProfilePicture);
+router.put("/updateCover", isAuthenticated, userController.updateCover);
+router.put("/updateDetails", isAuthenticated, userController.updateDetails);
+router.put("/addFriend/:id", isAuthenticated, userController.addFriend);
+router.put("/cancelRequest/:id", isAuthenticated, userController.cancelRequest);
+router.put("/follow/:id", isAuthenticated, userController.follow);
+router.put("/unfollow/:id", isAuthenticated, userController.unfollow);
+router.put("/unfriend/:id", isAuthenticated, userController.unfriend);
+router.put("/deleteRequest/:id", isAuthenticated, userController.deleteRequest);
+router.get("/getFriendsPageInfos", isAuthenticated, userController.getFriendsPageInfos);
 
 export default router;
