7076228c3d67813ab5448bdf253eb8bc3271197a
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-12-06 18:13:15 +0100

    Add PostReactions routes

---
+++ backend/src/controllers/reaction/reaction.ts
@@ -0,0 +1,97 @@
+import { Response } from "express";
+import mongoose from "mongoose";
+import { Reaction } from "../../models/reaction/Reaction";
+import User from "../../models/user/User";
+import { createErrorResponse,createSuccussResponse } from "../../helpers";
+import { RequestWithUserId } from "../../types/types";
+
+
+export const handlePostReactionsByPostId = async (req: RequestWithUserId, res: Response): Promise<Response> => {
+  try {
+    const { postId, reaction } = req.body;
+
+    if (!postId || !reaction) {
+      return createErrorResponse(res, 400, "POST_ID_OR_REACT_MISSING", "Post ID or reaction type is missing.");
+    }
+
+    // Check if the user has already reacted to the post
+    const existingReact = await Reaction.findOne({
+      postRef: postId,
+      reactBy: new mongoose.Types.ObjectId(req.user?.id),
+    });
+
+    if (!existingReact) {
+      // If no reaction found, create a new one
+      const newReact = new Reaction({
+        reaction: reaction,
+        postRef: postId,
+        reactBy: req.user?.id,
+      });
+      await newReact.save();
+
+      return createSuccussResponse(res, 200, "REACTION_ADDED", "Reaction added successfully.", newReact);
+    } else {
+      // If reaction is already present, update or remove the reaction
+      if (existingReact.reaction === reaction) {
+        await Reaction.findByIdAndDelete(existingReact._id);
+        return createSuccussResponse(res, 200, "REACTION_REMOVED", "Reaction removed successfully");
+      } else {
+        await Reaction.findByIdAndUpdate(existingReact._id, { reaction: reaction });
+        return createSuccussResponse(res, 200, "REACTION_UPDATED", "Reaction updated successfully.");
+      }
+    }
+  } catch (error: unknown) {
+    const errorMessage = (error as Error).message || "An unexpected error occurred. Please try again later.";
+    return createErrorResponse(res, 500, "SERVER_ERROR", errorMessage);
+  }
+};
+
+export const getReactions = async (req: RequestWithUserId, res: Response): Promise<Response> => {
+    try {
+        const postId = req.params.id;
+        // Fetch all reactions for the post
+        const reactsArray = await Reaction.find({ postRef: postId });
+    
+        // Group reactions by type (like, love, haha, etc.)
+        const groupedReacts = reactsArray.reduce<Record<string, typeof reactsArray>>(
+          (group, reaction) => {
+            const key = reaction.reaction;
+            group[key] = group[key] || [];
+            group[key].push(reaction);
+            return group;
+          },
+          {}
+        );
+  
+      // Prepare the final reactions count response
+      const reactions = [
+        { reaction: "like", count: groupedReacts.like ? groupedReacts.like.length : 0 },
+        { reaction: "love", count: groupedReacts.love ? groupedReacts.love.length : 0 },
+        { reaction: "haha", count: groupedReacts.haha ? groupedReacts.haha.length : 0 },
+        { reaction: "sad", count: groupedReacts.sad ? groupedReacts.sad.length : 0 },
+        { reaction: "wow", count: groupedReacts.wow ? groupedReacts.wow.length : 0 },
+        { reaction: "angry", count: groupedReacts.angry ? groupedReacts.angry.length : 0 },
+      ];
+  
+      // Check if the current user has reacted to this post
+      const userReaction = await Reaction.findOne({
+        postRef: postId,
+        reactBy: req.user?.id,
+      });
+  
+      // Check if the current user has saved the post
+      const user = await User.findById(req.user?.id);
+      const isPostSaved = user?.savedPosts.some((savedPost) => savedPost.post.toString() === postId);
+      const  data = {
+        reactions,
+        userReaction: userReaction?.reaction,
+        totalReacts: reactsArray.length,
+        isPostSaved: Boolean(isPostSaved),
+      };
+      return createSuccussResponse(res,200, "REACTION_DATA","Reactions retrieved successfully.",data)
+    } catch (error: unknown) {
+      const errorMessage = (error as Error).message || "An unexpected error occurred. Please try again later.";
+      return createErrorResponse(res, 500, "SERVER_ERROR", errorMessage);
+    }
+  };
+  
---
+++ backend/src/routes/reaction.ts
@@ -0,0 +1,11 @@
+import express, { Router } from "express";
+import * as reactionController from '../controllers/reaction/reaction';
+import { isAuthenticated } from "../middlewares/authenticate";
+
+
+const router: Router = express.Router();
+
+router.put("/post-reaction", isAuthenticated, reactionController.handlePostReactionsByPostId);
+router.get("/getPostReactions/:id", isAuthenticated, reactionController.getReactions);
+
+export default router;
