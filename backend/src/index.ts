77da2affb50a354362feb800d5f2ebbea7b85a30
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-12-07 17:46:37 +0100

    add Frontend url in  enivrement variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 app.use(express.static(path.join(__dirname, "src", "public")));
 app.use(express.json());
 const corsOptions = {
-  origin: "http://localhost:3000", // Your frontend origin
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Your frontend origin
   credentials: true, // Allow credentials (cookies, etc.)
   optionsSuccessStatus: 200, // Status for preflight responses
 };
