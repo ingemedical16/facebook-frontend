fa2951f00ad4d9947435cbb2baacfe801d2ddb68
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-12-19 20:50:58 +0100

    update database URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  process.env.BACKEND_URL || "https://facebook-backend-8n2p.onrender.com/api";
+  process.env.BACKEND_URL || "https://facebook-backend-2f6a.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
