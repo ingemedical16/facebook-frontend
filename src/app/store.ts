6a903568d973f45158a951d9a7d077a01da3cfdc
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-12-29 13:15:49 +0100

    update the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,12 +5,14 @@ import themeReducer from "../features/slices/theme/themeSlice";
 import searchReducer from "../features/slices/search/searchSlice";
 import postsReducer from "../features/slices/posts/postsSlice";
 import profileReducer from "../features/slices/profile/profileSlice";
+import friendsReducer from "../features/slices/friends/friendsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     posts:postsReducer,
     user: userReducer,
+    friends:friendsReducer,
     profile:profileReducer,
     theme: themeReducer,
     search: searchReducer,
