d5eb888368f4e6a7d5710076476333301d64d20b
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-01-24 12:10:09 +0200

    update right side components

--- src/components/home/right/index.tsx
+++ src/components/home/right/index.tsx
@@ -1,32 +1,14 @@
 import { FC } from "react";
-import { Dots, NewRoom, Search } from "../../svg";
 import styles from "./RightHome.module.css";
-import ContactsList from "./contactsList";
+import ContactsWrap from "../../chat/contactsWrap";
 
 const RightHome: FC = () => {
-  const color = "#65676b";
+ 
   return (
     <div className={styles.right_home}>
       <div className={styles.heading}>Sponsored</div>
       <div className={styles.splitter1}></div>
-      <div className={styles.contacts_wrap}>
-        <div className={styles.contacts_header}>
-          <div className={styles.contacts_header_left}>Contacts</div>
-          <div className={styles.contacts_header_right}>
-            <div className={`${styles.contact_circle} hover1`}>
-              <NewRoom color={color} />
-            </div>
-            <div className={`${styles.contact_circle} hover1`}>
-              <Search color={color} />
-            </div>
-            <div className={`${styles.contact_circle} hover1`}>
-              <Dots color={color} />
-            </div>
-          </div>
-        </div>
-
-        <ContactsList />
-      </div>
+      <ContactsWrap/>
     </div>
   );
 };
